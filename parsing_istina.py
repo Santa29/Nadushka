7345352ed0efa029629a48e2136ed2b2fee5884c
Mikhail Rybolovlev <michaels17525@mail.ru>
2020-11-06 14:16:00 +0300

    rewritw parsing function

--- parsing_istina.py
+++ parsing_istina.py
@@ -6,6 +6,20 @@ resp = req.get("https://istina.msu.ru/profile/andnadya/")
 soup = BeautifulSoup(resp.text, 'lxml')
 
 list_of_a_tags = []
-for el in soup.find_all("a"):
+list_of_publications = []
+list_of_authors = []
+list_of_jornals = []
+for el in soup.find_all("a", href=True):
     list_of_a_tags.append(el)
-    print(el)
+
+i=-1
+for el in list_of_a_tags:
+    print(el.text)
+    if "publications" in el.text:
+        list_of_publications.append(el)
+        i+=1
+    elif "workers" in el.text:
+            list_of_authors[i].append(el)
+    elif "jornals" in el.text:
+        list_of_jornals.append(el)
+print(list_of_authors, list_of_jornals, list_of_publications)

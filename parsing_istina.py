37d42d3204e30818df0a0e295e82610a5df10de6
Michael <bsvbdf6t43q>
2020-11-16 20:38:13 +0300

    Rewrite database to parsing the articles from istina

--- parsing_istina.py
+++ parsing_istina.py
@@ -1,25 +1,41 @@
 from bs4 import BeautifulSoup
 import requests as req
 
+class Publication():
+    list_of_authors = []
+    jornal = ''
+    ref = ''
+
 resp = req.get("https://istina.msu.ru/profile/andnadya/")
 
 soup = BeautifulSoup(resp.text, 'lxml')
 
 list_of_a_tags = []
-list_of_publications = []
-list_of_authors = []
-list_of_jornals = []
 for el in soup.find_all("a", href=True):
     list_of_a_tags.append(el)
 
-i=-1
+
+i = 0
 for el in list_of_a_tags:
-    print(el.text)
-    if "publications" in el.text:
-        list_of_publications.append(el)
+    if "publications" in el['href']:
         i+=1
-    elif "workers" in el.text:
-            list_of_authors[i].append(el)
-    elif "jornals" in el.text:
-        list_of_jornals.append(el)
-print(list_of_authors, list_of_jornals, list_of_publications)
+publication_list = []
+for public in range(i):
+    publication_list.append(Publication())
+
+
+counter = -1
+for el in list_of_a_tags:
+    if "publications" in el['href']:
+        counter+=1
+        publication_list[counter].ref = el['href']
+    elif "workers" in el['href'] and "style" not in el['href']:
+        publication_list[counter].list_of_authors.append(el['href'])
+    elif "jornals" in el["href"]:
+        publication_list[counter].jornal = el['href']
+
+for el in publication_list:
+    print(el.ref + '\n')
+    print(el.jornal)
+    el.list_of_authors = set(el.list_of_authors)
+    print(el.list_of_authors)

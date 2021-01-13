0cbd700eb92af0b46326812bd41c4be96c54cfd4
Mikhail Rybolovlev <michaels17525@mail.ru>
2021-01-13 16:10:16 +0300

    Wrtie functionality to get the pubmed articles data

--- parsing_pubmed.py
+++ parsing_pubmed.py
@@ -3,13 +3,13 @@ import requests as req
 import time
 
 class Publication():
-    list_of_authors = []
-    jornal = ''
-    ref = ''
-    article_id = ''
-    article_url = ''
+    def __init__(self, list, jornal, ref, id):
+        self.list_of_authors = list
+        self.jornal = jornal
+        self.article_ref = ref
+        self.article_id = id
 
-def get_MCID(list_of_terms):
+def get_MCID_and_number_of_publications(list_of_terms):
     base = 'https://eutils.ncbi.nlm.nih.gov/entrez/eutils/esearch.fcgi?db=pubmed&term='
     tail = '&usehistory=y&email=michaels17525@mail.ru'
     dict_of_WebEnv = {}
@@ -17,10 +17,31 @@ def get_MCID(list_of_terms):
         time.sleep(2)
         resp = req.get(base + el + tail)
         soup = BeautifulSoup(resp.text, 'xml')
-        dict_of_WebEnv[el] = soup.find('WebEnv').text
+        dict_of_WebEnv[el] = [soup.find('WebEnv').text, soup.find('Count').text]
     return dict_of_WebEnv
 
+def get_articles(list_of_MCID):
+    base = "https://eutils.ncbi.nlm.nih.gov/entrez/eutils/efetch.fcgi?db=pubmed&WebEnv="
+    tail = "&query_key=1&rettype=abstract"
+    temporary_list_of_articles = []
+    for el in list_of_MCID.keys():
+        time.sleep(2)
+        resp = req.get(base + el[0] + tail)
+        soup = BeautifulSoup(resp.text, 'xml')
+        temporary_list_of_articles.append(soup)
+    return temporary_list_of_articles
 
+def create_list_of_articles(temporary_list, list_of_MCID):
+    pass
+
+def connect_to_db_and_read_data():
+    pass
+
+def update_the_db():
+    pass
+
+def update_log_file():
+    pass
 
 list_of_referenses_to_parse = [
     "Andrianova+NV",
@@ -40,5 +61,5 @@ publication_list = []
 publication_list_links = []
 
 if __name__ == "__main__":
-    dict_of_authors_ref = get_MCID(list_of_referenses_to_parse)
+    dict_of_authors_ref = get_MCID_and_number_of_publications(list_of_referenses_to_parse)
     print(dict_of_authors_ref)
